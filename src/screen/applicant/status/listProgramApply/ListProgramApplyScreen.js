c159dc4661149bb6142ef2e3c4a36fd089cd76e8
gde1indraraditya <gde1indraraditya@gmail.com>
2022-03-27 16:25:53 +0700

    Edit home screen if there are not applied job

--- src/screen/applicant/status/listProgramApply/ListProgramApplyScreen.js
+++ src/screen/applicant/status/listProgramApply/ListProgramApplyScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {Fragment, useContext, useEffect} from 'react';
+import {goToLogin, goToScreen} from '../../../../navigation/NavigationHelper';
+import {VACANY_PATH} from '../../../../navigation/NavigationPath';
 import jwt_decode from 'jwt-decode';
 import {
   Image,
@@ -17,6 +19,9 @@ const ListProgramApplyScreen = ({bloc}) => {
   const {list, getListAppliedProgram, goToDetailStatus, navigate, loading} =
     bloc();
   const isLogin = useSelector(state => state.TechconnectAcademyReducer.isLogin);
+  const toVacany = () => {
+    goToScreen(VACANY_PATH, true);
+  };
   console.log('ini list program info');
   console.log(list.ProgramInfo);
 
@@ -44,14 +49,49 @@ const ListProgramApplyScreen = ({bloc}) => {
     });
   return (
     <SafeAreaView style={styles.program}>
-      <Text style={styles.programApplied}>Program</Text>
-      <Text style={styles.programApplied}>Applied</Text>
-      <View style={styles.listAppliedProgram}>{listAppliedProgram}</View>
+      {list?.ProgramInfo !== null ? (
+        <View>
+          <Text style={styles.programApplied}>Program</Text>
+          <Text style={styles.programApplied}>Applied</Text>
+          <View style={styles.listAppliedProgram}>{listAppliedProgram}</View>
+        </View>
+      ) : (
+        <View>
+          <Text style={styles.oops}>OOPS!!!</Text>
+          <Text style={styles.dontHaveProgram}>You don't have any</Text>
+          <Text style={styles.dontHaveProgram}>Applied Program yet</Text>
+          <Image
+            style={styles.image}
+            source={require('../../../../assets/images/emptyJobApplied.png')}
+            alt="splash-screen"
+          />
+          <TouchableOpacity style={styles.button} onPress={() => toVacany()}>
+            <Text style={styles.text}> Go To Program List </Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  text: {
+    textAlign: 'center',
+    color: 'white',
+  },
+  button: {
+    backgroundColor: '#E60965',
+    alignItems: 'center',
+    margin: 12,
+    padding: 12,
+    borderRadius: 50,
+    color: 'white',
+  },
+  image: {
+    width: null,
+    resizeMode: 'contain',
+    height: 300,
+  },
   listAppliedProgram: {
     marginTop: 20,
   },
@@ -63,6 +103,22 @@ const styles = StyleSheet.create({
   details: {
     color: '#FFFFFF',
     alignSelf: 'center',
+  },
+  dontHaveProgram: {
+    fontFamily: 'Montserrat',
+    fontSize: 36,
+    alignItems: 'center',
+    alignSelf: 'center',
+    alignContent: 'center',
+    color: '#CE49BF',
+  },
+  oops: {
+    fontFamily: 'Montserrat',
+    fontSize: 36,
+    alignItems: 'center',
+    alignSelf: 'center',
+    alignContent: 'center',
+    color: '#E60965',
   },
   programApplied: {
     fontFamily: 'Montserrat',
--- src/screen/splash/SplashScreen.js
+++ src/screen/splash/SplashScreen.js
@@ -1,6 +1,12 @@
-import React, { useEffect } from "react";
-import { SafeAreaView, StyleSheet, Image, Text, ActivityIndicator } from "react-native";
-import { goToLogin } from "../../navigation/NavigationHelper";
+import React, {useEffect} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  Text,
+  ActivityIndicator,
+} from 'react-native';
+import {goToLogin} from '../../navigation/NavigationHelper';
 
 const SplashScreen = () => {
   useEffect(() => {
@@ -11,18 +17,18 @@ const SplashScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>
-        Find Your{"\n"}
+        Find Your{'\n'}
         <Text style={styles.spanTitle}>Dream Career</Text>
       </Text>
-      <Text style={{ marginLeft: 50, fontWeight: "600", fontSize: 16 }}>
+      <Text style={{marginLeft: 50, fontWeight: '600', fontSize: 16}}>
         in Techconnect Academy
       </Text>
       <Image
-        style={{ alignSelf: "center" }}
-        source={require("../../assets/images/intro.png")}
+        style={{alignSelf: 'center'}}
+        source={require('../../assets/images/intro.png')}
         alt="splash-screen"
       />
-        <ActivityIndicator />
+      <ActivityIndicator />
     </SafeAreaView>
   );
 };
@@ -30,19 +36,19 @@ const SplashScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#ECE1EE",
-    justifyContent: "center",
+    backgroundColor: '#ECE1EE',
+    justifyContent: 'center',
   },
   title: {
     fontSize: 36,
-    color: "#666666",
-    fontWeight: "600",
+    color: '#666666',
+    fontWeight: '600',
     marginLeft: 20,
   },
   spanTitle: {
     fontSize: 36,
-    color: "#725AA4",
-    fontWeight: "600",
+    color: '#725AA4',
+    fontWeight: '600',
   },
 });
 
